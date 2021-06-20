6494bfdc50e4975b75562800e0a91f9b46810ede
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2021-06-20 23:50:01 +0530

    moongose removed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 require('dotenv/config')
 const app = express()
-const mongoose = require("mongoose")
+// const mongoose = require("mongoose")
 const jwt = require('./helpers/jwt')
 
 const cors = require('cors')
@@ -13,14 +13,13 @@ app.use(bodyparser.json())
 app.use(jwt())
 
 // Route import
-const userRoute = require('./routes/user')
-const movieRoute = require('./routes/movie')
+// const userRoute = require('./routes/user')
 const loginRoute = require('./routes/login')
 
 
 app.use('/login', loginRoute)
-app.use('/user', userRoute)
-app.use('/movie', movieRoute)
+// app.use('/user', userRoute)
+
 
 
 
@@ -31,14 +30,17 @@ app.get('/', (req,res) => {
 
 
 
-mongoose.connect( process.env.DB_CONNECTION,
-    {useNewUrlParser: true ,useUnifiedTopology: true} , 
-    ()=>{console.log("conncted to db");}
-)
-var db = mongoose.connection;
+// mongoose.connect( process.env.DB_CONNECTION,
+//     {useNewUrlParser: true ,useUnifiedTopology: true} , 
+//     ()=>{console.log("conncted to db");}
+// )
+// var db = mongoose.connection;
 
-//Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// //Bind connection to error event (to get notification of connection errors)
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+
 
 //SERVER
 app.listen(3002)
