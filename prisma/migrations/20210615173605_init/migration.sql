e0944f766701dbef02f9e2748be5eed1d6997f96
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2021-07-13 13:00:27 +0530

    m

--- prisma/migrations/20210615173605_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE `User` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR(20) NOT NULL,
-    `password` VARCHAR(20) NOT NULL,
-    `role` ENUM('STUDENT', 'TEACHER', 'PRINCIPAl', 'SCHOOLADMIN', 'ADMIN') NOT NULL DEFAULT 'STUDENT',
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
