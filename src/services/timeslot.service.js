89fac9ba663d6870129b61cee7fc489685bc6667
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2021-07-29 23:29:04 +0530

    ts service

--- src/services/timeslot.service.js
+++ src/services/timeslot.service.js
@@ -79,13 +79,110 @@ async function getTimeSlotsForSclAdmin(schoolid, grade, classname) {
   return data;
 }
 
-async function createTimeslot(schoolid, level) {
-  const periods = await prisma.period_time.findMany({});
+async function createTimeslot(data, schoolid) {
+  const isConflicts = await isConflictsWithTeacher(data, schoolid);
+
+  if (!isConflicts) {
+    const newts = await prisma.time_slot.create({
+      data: {
+        teacher_id: data.teacher_id,
+        weekday: data.weekday,
+        class_id: data.class_id,
+        peroid_id: data.period_id,
+        sdid: data.sd_id,
+      },
+    });
+    return newts;
+  } else {
+    throw {
+      status: 500,
+      message:
+        "Cannot assign the timeslot because teacher has a another subject to teach at this time",
+    };
+  }
+}
+
+async function updateTimeslot(data, schoolid, ts_id) {
+  const isConflicts = await isConflictsWithTeacher(data, schoolid);
+
+  if (!isConflicts) {
+    const newts = await prisma.time_slot.update({
+      where: {
+        id: ts_id,
+      },
+      data: {
+        teacher_id: data.teacher_id,
+        weekday: data.weekday,
+        class_id: data.class_id,
+        peroid_id: data.period_id,
+        sdid: data.sd_id,
+      },
+    });
+    return newts;
+  } else {
+    throw {
+      status: 500,
+      message:
+        "Cannot assign the timeslot because teacher has a another subject to teach at this time",
+    };
+  }
+}
+
+async function isConflictsWithTeacher(data, schoolid) {
+  const currntperiod = await prisma.period_time.findFirst({
+    where: {
+      id: data.period_id,
+    },
+    select: {
+      id: true,
+      starttime: true,
+      endtime: true,
+    },
+  });
+
+  const allperiods = await prisma.period_time.findMany({
+    where: {
+      school_id: schoolid,
+    },
+    select: {
+      id: true,
+      starttime: true,
+      endtime: true,
+    },
+  });
+
+  // find overlapping periods in different sections
+  var overlaps = [];
+  allperiods.forEach((peroid) => {
+    const ps = peroid.starttime.getTime();
+    const pe = peroid.endtime.getTime();
+    const cs = currntperiod.starttime.getTime();
+    const ce = currntperiod.endtime.getTime();
+    if (!((ce <= ps && ce < pe) || (cs > ps && cs >= pe))) {
+      overlaps.push(peroid.id);
+    }
+  });
+  console.log(overlaps);
+
+  const conflicts = await prisma.time_slot.findMany({
+    where: {
+      teacher_id: data.teacher_id,
+      weekday: data.weekday,
+      peroid_id: {
+        in: overlaps,
+      },
+    },
+  });
+
+  console.log(conflicts);
+
+  return conflicts.length !== 0;
 }
 
 const timeslotservice = {
   getTimeSlotsForSclAdmin,
   createTimeslot,
+  updateTimeslot,
 };
 
 module.exports = timeslotservice;
