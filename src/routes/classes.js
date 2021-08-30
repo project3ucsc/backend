69a056f8158e5427a5262731ba3ff71bf9de409f
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2021-08-30 18:19:55 +0530

    student enroll

--- src/routes/classes.js
+++ src/routes/classes.js
@@ -13,6 +13,18 @@ Router.post("/addall/:sclid", async (req, res) => {
   }
 });
 
+// return class for id
+Router.get("/:classid", async (req, res) => {
+  try {
+    const data = await classservice.getClass(parseInt(req.params.classid));
+    res.json(data);
+  } catch (err) {
+    res
+      .status(err.status || 500)
+      .json({ status: err.status, message: err.message });
+  }
+});
+
 // return no of classes for each section
 Router.get("/num/:sclid", async (req, res) => {
   try {
@@ -39,6 +51,47 @@ Router.get("/getSubDetailsforTeacher/:userid", async (req, res) => {
   }
 });
 
+// return enroal status of a student
+Router.get("/enrollStatus/:userid", async (req, res) => {
+  try {
+    const data = await classservice.getsStudentEnrollStatus(req.params.userid);
+    res.json(data);
+  } catch (err) {
+    res
+      .status(err.status || 500)
+      .json({ status: err.status, message: err.message });
+  }
+});
+
+// return enroal status of a student
+Router.post("/enroll/:userid", async (req, res) => {
+  try {
+    const data = await classservice.enrollStudent(
+      parseInt(req.params.userid),
+      req.body
+    );
+    res.json(data);
+  } catch (err) {
+    res
+      .status(err.status || 500)
+      .json({ status: err.status, message: err.message });
+  }
+});
+
+Router.delete("/unenroll/:classid/:userid", async (req, res) => {
+  try {
+    const data = await classservice.unenrollStudent(
+      parseInt(req.params.classid),
+      parseInt(req.params.userid)
+    );
+    res.json(data);
+  } catch (err) {
+    res
+      .status(err.status || 500)
+      .json({ status: err.status, message: err.message });
+  }
+});
+
 // return subject details for teacherRouter
 Router.get("/getSubDetailsforStudent/:userid", async (req, res) => {
   try {
@@ -56,6 +109,21 @@ Router.get("/getSubDetailsforStudent/:userid", async (req, res) => {
 Router.get("/getdetails/:school/:grade/:classno", async (req, res) => {
   try {
     const data = await classservice.getdetails(
+      parseInt(req.params.school),
+      req.params.grade,
+      req.params.classno
+    );
+    res.json(data);
+  } catch (err) {
+    res
+      .status(err.status || 500)
+      .json({ status: err.status, message: err.message });
+  }
+});
+
+Router.get("/getSDsinClass/:school/:grade/:classno", async (req, res) => {
+  try {
+    const data = await classservice.getSDsinClass(
       parseInt(req.params.school),
       req.params.grade,
       req.params.classno
--- src/services/classes.service.js
+++ src/services/classes.service.js
@@ -1,4 +1,10 @@
-const { PrismaClient, acc_status, user_role } = require("@prisma/client");
+const {
+  PrismaClient,
+  acc_status,
+  user_role,
+  student_datail_status,
+  subjectgroup,
+} = require("@prisma/client");
 const prisma = new PrismaClient();
 // names = ['A','B','C','D','E','F','F','H','I',   ]
 async function addall(values, schoolid) {
@@ -34,6 +40,99 @@ async function addall(values, schoolid) {
   return { succes: "200" };
 }
 
+async function getsStudentEnrollStatus(userid) {
+  const st = await prisma.studentdetail.findFirst({
+    where: {
+      user_id: parseInt(userid),
+    },
+    select: {
+      status: true,
+      classid: true,
+    },
+  });
+
+  if (!st) return { status: student_datail_status.NO_ENROll };
+  return st;
+}
+
+async function unenrollStudent(classid, userid) {
+  const std = await prisma.studentdetail.findFirst({
+    where: {
+      classid: classid,
+      user_id: userid,
+      status: student_datail_status.PENDING,
+    },
+  });
+  if (!std) throw { status: 404, message: "Failed to unenroll" };
+
+  const ops = await prisma.optionalsubs.deleteMany({
+    where: {
+      studentdetail_id: std.id,
+    },
+  });
+  console.log(ops);
+  const deletedstd = await prisma.studentdetail.delete({
+    where: {
+      id: std.id,
+    },
+  });
+  return deletedstd;
+}
+
+async function enrollStudent(userid, data) {
+  const std = await prisma.studentdetail.create({
+    data: {
+      user_id: userid,
+      classid: data.classid,
+      // regid:data.regid,
+      status: student_datail_status.PENDING,
+    },
+  });
+
+  if (data.section === "69") {
+    const ops = await prisma.optionalsubs.create({
+      data: {
+        sd_id: data.OPTIONAL_69,
+        studentdetail_id: std.id,
+        subjectgroup: subjectgroup.OPTIONAL_69,
+      },
+    });
+  } else if (data.section === "OL") {
+    const ops = await prisma.optionalsubs.createMany({
+      data: [
+        {
+          sd_id: data.OL_BUCKET_1,
+          studentdetail_id: std.id,
+          subjectgroup: subjectgroup.OL_BUCKET_1,
+        },
+        {
+          sd_id: data.OL_BUCKET_2,
+          studentdetail_id: std.id,
+          subjectgroup: subjectgroup.OL_BUCKET_2,
+        },
+        {
+          sd_id: data.OL_BUCKET_3,
+          studentdetail_id: std.id,
+          subjectgroup: subjectgroup.OL_BUCKET_3,
+        },
+      ],
+    });
+  } else if (data.section === "AL") {
+    if (data.OPTIONAL_AL) {
+      const [sdid, subgroup] = data.OPTIONAL_AL.split(".");
+      const ops = await prisma.optionalsubs.create({
+        data: {
+          sd_id: parseInt(sdid),
+          studentdetail_id: std.id,
+          subjectgroup: subgroup,
+        },
+      });
+    }
+  }
+
+  // if (!st) return { status: student_datail_status.NO_ENROll };
+  return std;
+}
 async function getSubDetailsForteacherRouter(userid) {
   const subs = await prisma.subject_detail.findMany({
     where: {
@@ -65,11 +164,11 @@ async function getSubDetailsForStudentRouter(userid) {
   const studetail = await prisma.studentdetail.findFirst({
     where: {
       user_id: parseInt(userid),
+      status: student_datail_status.ACTIVE,
     },
   });
 
-  if (!studetail)
-    throw { status: 404, message: "Student has not enrolled to a class" };
+  if (!studetail) throw { status: 404, message: "notenrolled" };
 
   const subs = await prisma.subject_detail.findMany({
     where: {
@@ -88,6 +187,16 @@ async function getSubDetailsForStudentRouter(userid) {
   if (!subs) throw { status: 404, message: "no data" };
 
   return subs;
+}
+
+async function getClass(classid) {
+  const classroom = await prisma.classroom.findFirst({
+    where: {
+      id: parseInt(classid),
+    },
+  });
+  if (!classroom) throw { status: 404, message: "Class Not Found" };
+  return classroom;
 }
 
 async function getno_of_classes(schoolid) {
@@ -109,6 +218,38 @@ async function getno_of_classes(schoolid) {
   });
   if (!schoolsectiondetail) throw { status: 404, message: "Details Not Found" };
   return schoolsectiondetail;
+}
+
+async function getSDsinClass(schoolid, grade, name) {
+  const allsubjectdetails = await prisma.classroom.findFirst({
+    where: {
+      AND: {
+        school_id: schoolid,
+        grade: grade,
+        name: name,
+      },
+    },
+    select: {
+      id: true,
+
+      subject_detail: {
+        select: {
+          id: true,
+          subject: {
+            select: {
+              id: true,
+              name: true,
+              subjectgroup: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  if (!allsubjectdetails) throw { status: 404, message: "Class not found" };
+
+  return allsubjectdetails;
 }
 
 async function getdetails(schoolid, grade, name) {
@@ -158,27 +299,7 @@ async function getdetails(schoolid, grade, name) {
         },
       },
     });
-    // select: {
-    //   id: true,
 
-    //   subject_detail: {
-    //     select: {
-    //       id: true,
-    //       subject: {
-    //         select: {
-    //           id: true,
-    //           name: true,
-    //         },
-    //       },
-    //       teacher: {
-    //         select: {
-    //           id: true,
-    //           username: true,
-    //         },
-    //       },
-    //     },
-    //   },
-    // },
     return { subjects, teachers, allsubjectdetails };
   } catch (error) {
     throw error;
@@ -223,10 +344,15 @@ async function patchSubjectDetail(sdid, data) {
 }
 
 const classservice = {
+  getClass,
   addall,
   getno_of_classes,
+  getsStudentEnrollStatus,
+  enrollStudent,
+  unenrollStudent,
   getSubDetailsForteacherRouter,
   getSubDetailsForStudentRouter,
+  getSDsinClass,
   getdetails,
   addSubjectDetail,
   deleteSubjectDetail,
