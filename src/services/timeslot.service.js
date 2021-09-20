62ca564417adb9cfa366f9cf8ed4ae31551421b8
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2021-09-20 23:25:33 +0530

    teacher timeline

--- src/services/timeslot.service.js
+++ src/services/timeslot.service.js
@@ -2,6 +2,19 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const { sectionmap } = require("../helpers/config");
+
+function getDateTxt(st, et) {
+  const timeoptions = {
+    hourCycle: "h23",
+    hour: "2-digit",
+    minute: "2-digit",
+  };
+  return `${st.toLocaleTimeString([], timeoptions)} - ${et.toLocaleTimeString(
+    [],
+    timeoptions
+  )}`;
+}
+
 async function getTimeSlotsForStudent(schoolid, studentid) {
   // const grade = "G12MATH";
   const studentdetail = await prisma.studentdetail.findFirst({
@@ -145,6 +158,31 @@ async function getTimeSlotsForSclAdmin(schoolid, grade, classname) {
   return data;
 }
 
+async function getTimeSlotsForTeacher(teacherid) {
+  const data = await prisma.time_slot.findMany({
+    where: { teacher_id: teacherid },
+    select: {
+      classroom: { select: { grade: true, name: true } },
+      subject_detail: {
+        select: { id: true, subject: { select: { name: true } } },
+      },
+      period_time: { select: { starttime: true, endtime: true } },
+      weekday: true,
+    },
+  });
+  const mappeddata = data.map((d) => {
+    return {
+      name: `${d.classroom.grade}-${d.classroom.name} | ${d.subject_detail.subject.name}`,
+      time: getDateTxt(d.period_time.starttime, d.period_time.endtime),
+      day: d.weekday,
+      sdid: d.subject_detail.id,
+    };
+  });
+  console.log(mappeddata);
+
+  return mappeddata;
+}
+
 async function createTimeslot(data, schoolid) {
   const isConflicts = await isConflictsWithTeacher(data, schoolid);
 
@@ -257,6 +295,7 @@ async function deleteTimeslot(ts_id) {
 const timeslotservice = {
   getTimeSlotsForSclAdmin,
   getTimeSlotsForStudent,
+  getTimeSlotsForTeacher,
   createTimeslot,
   updateTimeslot,
   deleteTimeslot,
